55bb2e784583fca1721abe5533c87e2c9937bbef
danielfr90 <danielfr90@users.noreply.github.com>
2018-02-21 19:40:49 +0100

    j

--- easyway/insert.php
+++ easyway/insert.php
@@ -1,9 +1,9 @@
 <?php
    $con=mysqli_connect("den1.mysql5.gear.host","catflyscore","Bn1F_75w1?tr","catflyscore");
-	$score1 = $_GET['Score1'];
-	$score2 = $_GET['Score2'];
-	$score3 = $_GET['Score3'];
-	$score4 = $_GET['Score4'];
+	$score1 = $_GET["Score1"];
+	$score2 = $_GET["Score2"];
+	$score3 = $_GET["Score3"];
+	$score4 = $_GET["Score4"];
    $sql="INSERT INTO globalhighscore (Score1, Score2, Score3, Score4) VALUES (" . $score1 . "," . $score2 . "," .$score3 . "," .$score4 . ")";
    if (mysqli_query($con,$sql)) {
       echo "Values have been inserted successfully";

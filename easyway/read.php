c250b3cdc7ca7fa3de7da5172882154ff92d5a96
danielfr90 <danielfr90@users.noreply.github.com>
2018-02-21 19:36:48 +0100

    new

--- easyway/read.php
+++ easyway/read.php
@@ -5,8 +5,6 @@
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
    }
 
-   $username = $_GET['username'];
-   $password = $_GET['password'];
    $result = mysqli_query($con,"SELECT * FROM globalhighscore");
    $row = mysqli_fetch_array($result);
    $data = $row[0];
--- insert.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $con=mysqli_connect("den1.mysql5.gear.host","catflyscore","Bn1F_75w1?tr","catflyscore");
-   $sql="INSERT INTO globalhighscore (Score1, Score2, Score3, Score4) VALUES (100, 100,100,100)";
-   if (mysqli_query($con,$sql)) {
-      echo "Values have been inserted successfully";
-   }
-	mysqli_close($con);
-?>

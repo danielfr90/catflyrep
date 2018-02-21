bd8120f05a2661c72b3724042056c711ce4c1815
danielfr90 <danielfr90@users.noreply.github.com>
2018-02-21 19:22:40 +0100

    corrected

--- read.php
+++ read.php
@@ -1,5 +1,5 @@
 <?php
-   $con=mysqli_connect("den1.mysql5.gear.host","catflyscore","Bn1F_75w1?tr","globalhighscore");
+   $con=mysqli_connect("den1.mysql5.gear.host","catflyscore","Bn1F_75w1?tr","catflyscore");
 
    if (mysqli_connect_errno($con)) {
       echo "Failed to connect to MySQL: " . mysqli_connect_error();

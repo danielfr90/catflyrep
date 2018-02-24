53e2ddd2af46a099c1124581ba0afa70db71c330
danielfr90 <danielfr90@users.noreply.github.com>
2018-02-24 02:11:05 +0100

    k

--- easyway/insert.php
+++ easyway/insert.php
@@ -1,18 +1,20 @@
 <?php
    $con=mysqli_connect("den1.mysql5.gear.host","catflyscore","Bn1F_75w1?tr","catflyscore");
-$sql="TRUNCATE TABLE globalhighscore";
+if (mysqli_connect_errno($con)) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+$sqla="TRUNCATE TABLE globalhighscore";
+$resulta = mysqli_query($con,$sqla);
 
+	
 
 	$score1 = $_POST["Score1"];
 	$score2 = $_POST["Score2"];
 	$score3 = $_POST["Score3"];
 	$score4 = $_POST["Score4"];
-   $sql="INSERT INTO globalhighscore (Score1, Score2, Score3, Score4) VALUES (" . $score1 . "," . $score2 . "," .$score3 . "," .$score4 . ")";
-$result = mysql_query($sql);	
+   $sqlb="INSERT INTO globalhighscore (Score1, Score2, Score3, Score4) VALUES (" . $score1 . "," . $score2 . "," .$score3 . "," .$score4 . ")";
+$resultb = mysqli_query($con,$sqlb);
 mysqli_close($con);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully". $score1 . $sql;
+
 ?>

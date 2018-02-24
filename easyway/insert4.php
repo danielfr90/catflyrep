9b1de252302b3865414afedc5adc8fe157594f5e
danielfr90 <danielfr90@users.noreply.github.com>
2018-02-24 02:41:19 +0100

    l

---
+++ easyway/insert4.php
@@ -0,0 +1,34 @@
+ <?php
+$servername = "den1.mysql5.gear.host";
+$username = "catflyscore";
+$password = "Bn1F_75w1?tr";
+$dbname = "catflyscore";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+//$sql = "INSERT INTO globalhighscore (Score1, Score2, Score3, Score4) VALUES (1,1,1,1)";
+
+$sqla="TRUNCATE TABLE globalhighscore;";
+
+
+$score1 = $_POST["Score1"];
+$score2 = $_POST["Score2"];
+$score3 = $_POST["Score3"];
+$score4 = $_POST["Score4"];
+	
+$sql= $sqla . "INSERT INTO globalhighscore (Score1, Score2, Score3, Score4) VALUES (" . $score1 . "," . $score2 . "," . $score3 . "," . $score4 . ")";
+
+if (mysqli_multi_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?> 

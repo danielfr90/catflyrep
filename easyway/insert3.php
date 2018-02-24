fa40a71e59b5997a3b4c940aa064768e35cb913a
danielfr90 <danielfr90@users.noreply.github.com>
2018-02-24 02:36:03 +0100

    l

--- easyway/insert3.php
+++ easyway/insert3.php
@@ -11,13 +11,6 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$score1 = $_POST["Score1"];
-	$score2 = $_POST["Score2"];
-$score3 = $_POST["Score3"];
-	$score4 = $_POST["Score4"];
-	
-   $sql="INSERT INTO globalhighscore (Score1, Score2, Score3, Score4) VALUES (" . $score1 . "," . $score2 . "," . $score3 . "," . $score4 . ")";
-
 
 //$sql = "INSERT INTO globalhighscore (Score1, Score2, Score3, Score4) VALUES (1,1,1,1)";
 
@@ -30,11 +23,24 @@ if (mysqli_query($conn, $sqla)) {
 }
 
 
-//if (mysqli_query($conn, $sql)) {
-//    echo "New record created successfully";
-//} else {
- //   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-//}
+$score1 = $_POST["Score1"];
+$score2 = $_POST["Score2"];
+$score3 = $_POST["Score3"];
+$score4 = $_POST["Score4"];
+	
+$sql="INSERT INTO globalhighscore (Score1, Score2, Score3, Score4) VALUES (" . $score1 . "," . $score2 . "," . $score3 . "," . $score4 . ")";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
 mysqli_close($conn);
 ?> 

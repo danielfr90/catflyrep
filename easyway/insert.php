6ad396114ac93e661effa4bfb3be15ca009bd8a8
danielfr90 <danielfr90@users.noreply.github.com>
2018-02-24 01:54:34 +0100

    j

--- easyway/insert.php
+++ easyway/insert.php
@@ -12,5 +12,5 @@ $sql="TRUNCATE TABLE globalhighscore";
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully". score1;
 ?>

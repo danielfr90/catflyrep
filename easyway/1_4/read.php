638405946dbdea927c1bbe2266c3e832725d7978
danielfr90 <danielfr90@users.noreply.github.com>
2018-03-01 18:20:36 +0100

    start work on 1 4

---
+++ easyway/1_4/read.php
@@ -0,0 +1,16 @@
+<?php
+   $con=mysqli_connect("den1.mysql5.gear.host","catflyscore","Bn1F_75w1?tr","catflyscore");
+
+   if (mysqli_connect_errno($con)) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+
+   $result = mysqli_query($con,"SELECT * FROM globalhighscorenew");
+   $row = mysqli_fetch_array($result);
+   $data = $row[0] . "," . $row[1] . "," .$row[2] . "," . $row[3];
+
+   if($data){
+      echo $data;
+   }
+   mysqli_close($con);
+?>
